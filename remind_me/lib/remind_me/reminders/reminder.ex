dec1325376c47af2062e4b085e893731d3288941
stebbinsi2 <stebbinsi2@gmail.com>
2023-08-14 17:47:10 -0400

    created user_auth table and boilerplate; migrations made for updating table with user_id

--- remind_me/lib/remind_me/reminders/reminder.ex
+++ remind_me/lib/remind_me/reminders/reminder.ex
@@ -4,14 +4,14 @@ defmodule RemindMe.Reminders.Reminder do
 
   schema "reminder" do
     field :content, :string
-
+    belongs_to :user, RemindMe.Accounts.User
     timestamps()
   end
 
   @doc false
   def changeset(reminder, attrs) do
     reminder
-    |> cast(attrs, [:content])
+    |> cast(attrs, [:content, :user_id])
     |> validate_required([:content])
   end
 end
--- remind_me/lib/remind_me_web/router.ex
+++ remind_me/lib/remind_me_web/router.ex
@@ -1,6 +1,8 @@
 defmodule RemindMeWeb.Router do
   use RemindMeWeb, :router
 
+  import RemindMeWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule RemindMeWeb.Router do
     plug :put_root_layout, html: {RemindMeWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -46,6 +49,44 @@ defmodule RemindMeWeb.Router do
 
       live_dashboard "/dashboard", metrics: RemindMeWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", RemindMeWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{RemindMeWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", RemindMeWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{RemindMeWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", RemindMeWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{RemindMeWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
