fcf1c36358b6437ff251d74a27f545fd1436ad2f
stebbinsi2 <stebbinsi2@gmail.com>
2023-08-21 16:59:02 -0400

    Added date selection to the form component

--- remind_me/lib/remind_me_web/live/reminder_live/form_component.ex
+++ remind_me/lib/remind_me_web/live/reminder_live/form_component.ex
@@ -19,7 +19,9 @@ defmodule RemindMeWeb.ReminderLive.FormComponent do
         phx-change="validate"
         phx-submit="save"
       >
+        <.input field={@form[:title]} type="text" label="Title" />
         <.input field={@form[:content]} type="text" label="Content" />
+        <.input field={@form[:remind_date]} type="date" label="Due Date" />
         <:actions>
         <.input field={@form[:user_id]} type="hidden" value={@current_user.id} />
           <.button phx-disable-with="Saving...">Save Reminder</.button>
